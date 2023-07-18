1c0394b9ee3cd3e0ee1b38e404bb46216706c578
Aswin S <aswinofficial3@gmail.com>
2023-07-18 23:09:13 +0530

    Splash screen added

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:simple_chat_app/screens/auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:simple_chat_app/screens/chat.dart';
+import 'package:simple_chat_app/screens/splash.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -29,6 +30,10 @@ class MyApp extends StatelessWidget {
       home: StreamBuilder(
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (ctx, snapshot) {
+          if(snapshot.connectionState == ConnectionState.waiting){
+            return const SplashScreen();
+          }
+
           if (snapshot.hasData) {
             return const ChatScreen();
           }
