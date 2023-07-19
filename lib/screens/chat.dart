0f668aabc72983fef063ee137eaa6dbee806fda8
Aswin S <aswinofficial3@gmail.com>
2023-07-19 23:11:47 +0530

    Added chatmessage and input widget

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:simple_chat_app/widgets/chat_messages.dart';
+import 'package:simple_chat_app/widgets/new_messages.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -14,15 +16,20 @@ class ChatScreen extends StatelessWidget {
             onPressed: () {
               FirebaseAuth.instance.signOut();
             },
-            icon:  Icon(
+            icon: Icon(
               Icons.exit_to_app,
               color: Theme.of(context).colorScheme.primary,
             ),
           )
         ],
       ),
-      body: const Center(
-        child: Text('Logged in'),
+      body: const Column(
+        children: [
+          Expanded(
+            child: ChatMessages(),
+          ),
+          NewMessages(),
+        ],
       ),
     );
   }
